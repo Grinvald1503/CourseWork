a267bbaa743469d11cc6ccae32a093ac3d257507
Aleksandr <aleksanches123@mail.ru>
2022-12-28 23:09:53 +0300

    конец

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println(palindrom("qweEWQ qweewq"));
 
+    }
+    public static boolean palindrom (String string) {
+        String revers = "";
+        for (int simbol = string.length() - 1; simbol >= 0; simbol--) {
+            revers = revers + string.charAt(simbol);
+        }
+        if (string.equalsIgnoreCase(revers)) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
